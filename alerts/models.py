338ac59ba3a93729659b007887812741c954baa7
Shrish-KS <86770411+Shrish-KS@users.noreply.github.com>
2023-09-25 20:49:42 +0530

    Update models.py

--- alerts/models.py
+++ alerts/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 from django.conf import settings
+from django.core.validators import MinLengthValidator
 
 # Create your models here.
 class Alertgroup(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, default=None, related_name="alertusers", on_delete=models.CASCADE)
-    name = models.CharField(max_length=5,default="AAPL")
+    name = models.CharField(max_length=5,validators=[MinLengthValidator(2)])
     alert = models.DecimalField(decimal_places=2,max_digits=7)
